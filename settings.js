ff521c267e31d5ded16b5d30b54b765b5abc7aa9
David Cyril <168988025+DeeCeeXxx@users.noreply.github.com>
2024-08-03 22:48:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,6 @@
 
 const fs = require('fs')
 const chalk = require('chalk')
-const fs = require("fs-extra");
-if (fs.existsSync(".env"))
-  require("dotenv").settings({ path: __dirname + "/.env" });
 //owmner v card
 global.ytname = "YouTube: David Cyril Tech" //ur yt chanel name
 global.socialm = "Telegram: t.me:DeeCee_x" //ur github or insta name
